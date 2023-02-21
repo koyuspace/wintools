68e0e9696325a4346f7c69176f5de78a84a27b55
koyu <me@koyu.space>
2023-02-21 10:51:49 +0100

    Change config

--- koyus-wintools/Properties/AssemblyInfo.cs
+++ koyus-wintools/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("koyu's WinTools")]
-[assembly: AssemblyCopyright("Copyright ©  koyu.space 2020")]
+[assembly: AssemblyCopyright("Copyright ©  koyu.space 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
